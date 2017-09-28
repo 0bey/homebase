fa0a0e408aeecf71fce29b048faf6c8c6664b36e
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2017-09-27 22:58:11 -0400

    data-driven experimenting

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Homebase from './components/stateful/Homebase.jsx';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Homebase />, div);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/stateful/App';
+import Homebase from './components/stateful/Homebase.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Homebase />, document.getElementById('root'));
 registerServiceWorker();
